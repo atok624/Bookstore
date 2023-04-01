ee3e12b5a52f0ede96c2bfb4498b941782e2f8ec
atok624 <84607674+atok624@users.noreply.github.com>
2023-04-01 10:11:15 +0000

    Create books.js file

---
+++ src/components/Books.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import MyBook from './BookItem';
+import Form from './BookForm';
+
+function Books() {
+  const Books = [
+    {
+      author: 'Kofi Adzor',
+      title: 'The Power of a woman',
+    },
+    {
+      author: 'Nicholas Amissah',
+      title: 'The Lion King',
+    },
+  ];
+  return (
+    <div className="book-container">
+      <div className="books-list">
+        {Books.map((book) => (
+          <div key={Books.indexOf(book)} className="book-item">
+            <div className="first-section">
+              <MyBook title={book.title} author={book.author} />
+              <br />
+              <div className="book-option">
+                <button type="button" className="comments-btn">
+                  Comments
+                </button>
+                <div className="divider" />
+                <button type="button" className="remove-btn">
+                  Removed
+                </button>
+                <div className="divider" />
+                <button type="button" className="edit-btn">
+                  Edit
+                </button>
+              </div>
+            </div>
+            <div className="second-section">
+              <p>50% completed</p>
+            </div>
+            <div className="divider" />
+            <div className="Third-section">
+              <p>CURRENT CHAPTER</p>
+              <p>CHAPTER 24</p>
+              <br />
+              <button type="button" className="update-progress">
+                UPDATE PROGRESS
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <hr />
+      <Form />
+    </div>
+  );
+}
+
+export default Books;
