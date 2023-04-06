3eafd7ec4c1e722889c8cb84d5d05acbc8ea0285
atok624 <84607674+atok624@users.noreply.github.com>
2023-04-06 09:03:14 +0000

    Create booksSlice.js file

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState: { list: [] },
+  reducers: {
+    addBook: (state, action) => ({
+      ...state,
+      list: [...state.list, action.payload],
+    }),
+    removeBook: (state, action) => ({
+      ...state,
+      list: state.list.filter((book) => book.id !== action.payload),
+    }),
+  },
+});
+
+export const { addBook, removeBook } = booksSlice.actions;
+
+export default booksSlice.reducer;
