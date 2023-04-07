5659d2460908da5936df86377e7ea5d1ec9df212
atok624 <84607674+atok624@users.noreply.github.com>
2023-04-07 21:32:50 +0000

    Add API to project

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,12 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import Book from './Books';
+import { getBooks } from '../redux/books/booksSlice';
 
 const BookList = () => {
   const { books } = useSelector((store) => store.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
+  // console.log(books);
   return (
     <ul>
       {books.map((book) => (
-        <Book key={book.id} book={book} />
+        <Book key={book.item_id} book={book} />
       ))}
     </ul>
   );
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,18 +1,53 @@
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { removeBook } from '../redux/books/booksSlice';
+import { deleteBook } from '../redux/books/booksSlice';
 
 const Books = ({ book }) => {
   const dispatch = useDispatch();
+  const randomPercent = Math.floor(Math.random() * 100);
   return (
-    <li>
-      <h2>{book.title}</h2>
-      <p>{book.author}</p>
-      <div className="book-actions-container">
-        <button type="button" onClick={() => dispatch(removeBook(book.id))}>
-          Remove
-        </button>
-        <button type="button">Edit</button>
+    <li className="book-card">
+      <div className="book-content">
+        <div className="book-info">
+          <span className="book-category">Action</span>
+          <span className="book-title">{book.title}</span>
+          <span className="book-author">{book.author}</span>
+        </div>
+        <div className="button-container">
+          <button type="button" className="interact-btn">
+            Comments
+          </button>
+          <div className="vertical-line" />
+          <button
+            type="button"
+            onClick={() => dispatch(deleteBook(book.item_id))}
+            className="interact-btn remove"
+          >
+            Remove
+          </button>
+          <div className="vertical-line" />
+          <button type="button" className="interact-btn">
+            Edit
+          </button>
+        </div>
+      </div>
+      <div className="progress-content">
+        <div className="progress-bar-container">
+          <div className="progress-bar" />
+          <div className="progress-text">
+            <span className="progress-percent">
+              {randomPercent}
+              %
+            </span>
+            <span className="progress-status">Completed</span>
+          </div>
+        </div>
+        <div className="progress-line-divider" />
+        <div className="book-chapter">
+          <span className="current-chapter-text">CURRENT CHAPTER</span>
+          <span className="chapter-text">Chapter 17</span>
+          <button type="button" className="update-progress">UPDATE PROGRESS</button>
+        </div>
       </div>
     </li>
   );
@@ -20,7 +55,7 @@ const Books = ({ book }) => {
 
 Books.propTypes = {
   book: PropTypes.shape({
-    id: PropTypes.string.isRequired,
+    item_id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
   }).isRequired,
